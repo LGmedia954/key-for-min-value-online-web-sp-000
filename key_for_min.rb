d068ff872c88702dd072d97f18f799044165749e
Leslie G <lesliegonzalez@me.com>
2020-07-13 03:59:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+tacos = {"chicken" => 2, "pork" => 2, "lamb" => 2, "cactus" => 1}
 
+
+def key_for_min_value(tacos)
+  tacos.collect do |choice, count|
+    if count < 2
+    return choice
+  else
+    return nil
+  end
+end
 end
