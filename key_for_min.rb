bb97fb57581687d4f9ea83e9662681afd0caa093
Leslie G <lesliegonzalez@me.com>
2020-07-13 15:04:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,12 @@ tacos = {"chicken" => 2, "pork" => 2, "lamb" => 2, "cactus" => 1}
 
 
 def key_for_min_value(tacos)
-  tacos.collect do |choice, count|
-    if count < 2
+  tacos.each do |choice, count|
+    if count < 2 || count == nil
     return choice
   else
     return nil
   end
 end
+return choice
 end
